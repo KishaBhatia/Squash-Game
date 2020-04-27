d8b72f9eb858a079aab11da3e80aa2f6170618f9
KishaBhatia <64302865+KishaBhatia@users.noreply.github.com>
2020-04-27 19:34:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,16 @@
-var ball,img,paddle;
+var ball,img,paddle,restart;
 var count=0;
 var time=0;
 
+var PLAY=1;
+var gameState=PLAY;
+var END=0;
+var gameState=END;
+
 function preload() {
-  ballimg = loadImage('ball.png');
+  ballimg = loadImage("ball.png");
   paddleimg=loadImage("paddle.png")
+  restartimg=loadImage("restart.jpg")
 }
 function setup() {
   createCanvas(400, 400);
@@ -12,8 +18,11 @@ function setup() {
   ball.addImage (ballimg); 
   ball.velocityX=9;  
   paddle=createSprite(350,200,20,100);
-  paddle.addImage(paddleimg)
-  
+  paddle.addImage(paddleimg);
+  restart=createSprite(190,220,20,100);
+  restart.addImage(restartimg);
+  restart.visible=false;
+  restart.scale=0.4;
 
 }
 
@@ -35,6 +44,10 @@ function draw() {
   line(95,45,295,45);
   
   edges=createEdgeSprites();
+  paddle.collide(edges);  
+  
+if(gameState===PLAY){
+  
   //Bounce Off the Left Edge only
   ball.bounceOff(edges[0]); 
   
@@ -42,8 +55,6 @@ function draw() {
   ball.bounceOff(edges[3]);
   
   ball.bounceOff(paddle,explosion);
-  
-  paddle.collide(edges);
   
   if(keyDown(UP_ARROW))
   {
@@ -56,18 +67,43 @@ function draw() {
   }
   
   if (ball.x>400){
-    time=0;
-    textFont("Algerian");
-    fill("green");
-    text("Game Over",150,200);
+    gameState=END;
   }
+  
+  if(mousePressedOver(restart)){
+      reset();
+    }
   
   if(frameCount%10===0){
   time=time +Math.round(getFrameRate()/30);
   }
   
+}
+  
+  else if(gameState===END){
+    ball.velocityX=0;
+    paddle.velocityY=0;
+    restart.visible=true;
+    time=0;
+    textFont("Algerian");
+    fill("green");
+    text("Game Over",150,200);
+
+    if(mousePressedOver(restart)){
+      reset();
+    }
+  }
   drawSprites();
   
+}
+
+function reset(){
+  gameState=PLAY;
+  restart.visible=false;
+  ball.x=60;
+  ball.y=200;
+  ball.velocityX=9;  
+  score=0;
 }
 
 function explosion(){
